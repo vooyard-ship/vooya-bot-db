43576f76cb5ab64642170c538817f2fc68a1811f
MAX1 <max1@MAX1ui-MacBookPro.local>
2026-04-15 08:38:29 +0900

    DB 업데이트 2026-04-15 08:38

--- sync.sh
+++ sync.sh
@@ -8,7 +8,7 @@ FILES=("menu_db.md" "fridge_db.md" "daycare_menu_2026_04.md")
 echo "DB 파일 감시 시작..."
 
 /opt/homebrew/bin/fswatch -o "/Users/max1/workspace/40-49_personal/47_family/menu_db.md" "/Users/max1/workspace/40-49_personal/47_family/fridge_db.md" "/Users/max1/workspace/40-49_personal/47_family/daycare_menu_2026_04.md" | while read; do
-    sleep 2  # 저장 완료 대기
+    sleep 5  # 저장 완료 대기 (iCloud 쓰기 완료 여유)
     changed=false
     for f in "${FILES[@]}"; do
         src="${DB_SOURCE}/${f}"
